e4f9fb661c51282fffdd466b11f7f430f32ed996
이효승 <julia_2000@naver.com>
2025-01-20 14:48:44 +0900

    init :  create vite

---
+++ king/src/App.jsx
@@ -0,0 +1,16 @@
+import kingLogo from '/logo.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src={kingLogo} className="logo" alt="Vite logo" />
+      </div>
+      <h1>촬영배경지 큐레이션 서비스, K-ing</h1>
+    </>
+  )
+}
+
+export default App
