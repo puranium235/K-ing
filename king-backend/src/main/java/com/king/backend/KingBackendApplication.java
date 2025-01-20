0af66ac2bbd94e430edd83e7aa40ce9d3d710b5b
김상욱 <kimsangouk1020@gmail.com>
2025-01-20 19:28:02 +0900

    'update'

---
+++ king-backend/src/main/java/com/king/backend/KingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.king.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KingBackendApplication.class, args);
+    }
+
+}
---
+++ king-backend/src/test/java/com/king/backend/KingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.king.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
