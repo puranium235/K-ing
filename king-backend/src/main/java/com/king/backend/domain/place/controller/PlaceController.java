8c1d7b71da0db3d6579617930f669956ee737256
최은영 <harrysunae@naver.com>
2025-01-21 21:40:51 +0900

    feat: GlobalExceptionHandler에 로깅 추가

--- king-backend/src/main/java/com/king/backend/domain/place/controller/PlaceController.java
+++ king-backend/src/main/java/com/king/backend/domain/place/controller/PlaceController.java
@@ -3,6 +3,7 @@ package com.king.backend.domain.place.controller;
 import com.king.backend.domain.place.errorcode.PlaceErrorCode;
 import com.king.backend.global.exception.CustomException;
 import com.king.backend.global.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,17 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/place")
+@Slf4j
 public class PlaceController {
 
+    // 공통 응답 모듈 테스트
     @GetMapping("/success")
     public ResponseEntity<ApiResponse<String>> getSuccessResponse() {
-        return ResponseEntity
+        log.info("GET /place/success 요청 처리 시작");
+        ResponseEntity<ApiResponse<String>> response = ResponseEntity
                 .status(HttpStatus.OK)
                 .body(ApiResponse.success("Place found successfully!"));
+        log.info("GET /place/success 요청 처리 완료: {}", response.getBody());
+        return response;
     }
 
     @GetMapping("/error")
     public ResponseEntity<ApiResponse<String>> getErrorResponse() {
+        log.warn("GET /place/error 요청 처리 중 예외 발생");
         throw new CustomException(PlaceErrorCode.PLACE_NOT_FOUND);
     }
 
--- king-backend/src/main/java/com/king/backend/global/exception/GlobalExceptionHandler.java
+++ king-backend/src/main/java/com/king/backend/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.king.backend.global.exception;
 
 import com.king.backend.global.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,13 +10,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.net.BindException;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ApiResponse<Void>> handleCustomException(CustomException ex) {
         ErrorCode errorCode = ex.getErrorCode();
+
+        log.error("CustomException 발생: 코드 = {}, 메시지 = {}",
+                errorCode.getCode(), errorCode.getMessage());
+
         return ResponseEntity
-                .status(errorCode.getStatus()) // ErrorCode에서 상태 코드 가져오기
+                .status(errorCode.getStatus())
                 .body(ApiResponse.error(errorCode.getCode(), errorCode.getMessage()));
     }
 
