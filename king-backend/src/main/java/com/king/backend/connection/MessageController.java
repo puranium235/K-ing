692fcec130ea9e0febf9245930af4c80e08f160d
이하경 <puranium235@gmail.com>
2025-01-22 08:43:37 +0900

    feat: redis 연결

--- king-backend/src/main/java/com/king/backend/connection/MessageController.java
+++ king-backend/src/main/java/com/king/backend/connection/MessageController.java
@@ -1,6 +1,7 @@
 package com.king.backend.connection;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MessageController {
     private final MessageRepository messageRepository;
+    private final RedisUtil redisUtil;
 
     @GetMapping
     public List<Message> getAllMessages() {
@@ -20,5 +22,24 @@ public class MessageController {
     @PostMapping
     public Message postMessage(@RequestBody Message message) {
         return messageRepository.save(message);
+    }
+
+    @GetMapping("/redis-set")
+    public boolean SetRedis(@RequestParam String key, @RequestParam String value) {
+        try {
+            redisUtil.setValue(key, value);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @GetMapping("/redis-get")
+    public String getRedis(@RequestParam String key) {
+        try {
+            return redisUtil.getValue(key);
+        } catch (Exception e) {
+            return "값이 없습니다";
+        }
     }
 }
