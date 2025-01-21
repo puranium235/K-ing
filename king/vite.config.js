07515d7e6ee7e9ae12a31dee921cdf4e6f118907
이효승 <julia_2000@naver.com>
2025-01-22 08:54:01 +0900

    fix: svg 컴포넌트화 에러 수정

--- king/vite.config.js
+++ king/vite.config.js
@@ -1,8 +1,9 @@
+// import svgr from "vite-plugin-svgr";
+import svgr from "@svgr/rollup";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import svgrPlugin from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgrPlugin()],
+  plugins: [react(), svgr()],
 });
