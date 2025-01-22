ae9b00c082e90865fc924c9bfbf7539b94ab7279
이효승 <julia_2000@naver.com>
2025-01-22 10:49:25 +0900

    fix:  eslint 에러 수정

--- king/src/Router.jsx
+++ king/src/Router.jsx
@@ -1,20 +1,20 @@
-import { Suspense } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { RecoilRoot } from "recoil";
+import { Suspense } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
-import ArchivePage from "./pages/ArchivePage";
-import ChatbotPage from "./pages/ChatbotPage";
-import CurationPage from "./pages/CurationPage";
-import DramaDetailPage from "./pages/DramaDetailPage";
-import DramaPage from "./pages/DramaPage";
-import ErrorPage from "./pages/ErrorPage";
-import FeedPage from "./pages/FeedPage";
-import HomePage from "./pages/HomePage";
-import LandingPage from "./pages/LandingPage";
-import MyPage from "./pages/MyPage";
-import UploadPage from "./pages/UploadPage";
-import MapPage from "./pages/MapPage";
-import PlaceDetailPage from "./pages/PlaceDetailPage";
+import ArchivePage from './pages/ArchivePage';
+import ChatbotPage from './pages/ChatbotPage';
+import CurationPage from './pages/CurationPage';
+import DramaDetailPage from './pages/DramaDetailPage';
+import DramaPage from './pages/DramaPage';
+import ErrorPage from './pages/ErrorPage';
+import FeedPage from './pages/FeedPage';
+import HomePage from './pages/HomePage';
+import LandingPage from './pages/LandingPage';
+import MapPage from './pages/MapPage';
+import MyPage from './pages/MyPage';
+import PlaceDetailPage from './pages/PlaceDetailPage';
+import UploadPage from './pages/UploadPage';
 
 const Router = () => {
   return (
