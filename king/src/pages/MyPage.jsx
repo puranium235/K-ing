84df0ad686b8f0ffb62711075f189110b9050e39
이효승 <julia_2000@naver.com>
2025-01-22 09:04:52 +0900

    feat: 홈화면 구성성

---
+++ king/src/pages/MyPage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+import Home from "../components/Home/Home";
+
+const MyPage = () => {
+  return (
+    <>
+      <Home />
+    </>
+  );
+};
+
+export default MyPage;
+
+const St = {
+  Wrapper: styled.article`
+    background-color: ${({ theme }) => theme.colors.White};
+    color: ${({ theme }) => theme.colors.Gray0};
+    // ${({ theme }) => theme.fonts.Title3};
+  `,
+};
