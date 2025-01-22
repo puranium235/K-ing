da821a57adfe1b1de9f9c32164b24d14ae46fd55
김민경 <kimmg0708@naver.com>
2025-01-22 16:06:30 +0900

    #4 feat: archive favorites 리스트 구현

---
+++ king/src/assets/dummy/dummyDataArchive.js
@@ -0,0 +1,61 @@
+import sampleImage from './curationimg.png';
+
+export const curationsDummyData = [
+  {
+    id: 1,
+    image: sampleImage,
+    author: 'k-ing_Official',
+    title: '최애의 흔적을 찾아서 : BTS의 RM편',
+    bookmarked: true,
+  },
+  {
+    id: 2,
+    image: sampleImage,
+    author: 'hsmoon101',
+    title: '바닷가 근처 드라마 촬영지.zip',
+    bookmarked: true,
+  },
+  {
+    id: 3,
+    image: sampleImage,
+    author: 'hsmoon101',
+    title: '바닷가 근처 드라마 촬영지.zip',
+    bookmarked: true,
+  },
+  {
+    id: 4,
+    image: sampleImage,
+    author: 'hsmoon101',
+    title: '바닷가 근처 드라마 촬영지.zip',
+    bookmarked: true,
+  },
+  {
+    id: 5,
+    image: sampleImage,
+    author: 'hsmoon101',
+    title: '바닷가 근처 드라마 촬영지.zip',
+    bookmarked: true,
+  },
+  {
+    id: 6,
+    image: sampleImage,
+    author: 'hsmoon101',
+    title: '바닷가 근처 드라마 촬영지.zip',
+    bookmarked: true,
+  },
+];
+export const favoriteWorksDummyData = [
+  { id: 1, image: sampleImage, title: '검은 사제들' },
+  { id: 2, image: sampleImage, title: '1987' },
+  { id: 3, image: sampleImage, title: '갯마을 차차차' },
+  { id: 4, image: sampleImage, title: 'SKY캐슬' },
+  { id: 5, image: sampleImage, title: '시그널' },
+  { id: 6, image: sampleImage, title: '명량' },
+  { id: 7, image: sampleImage, title: '극한직업' },
+];
+export const favoritePeopleDummyData = [
+  { id: 1, image: sampleImage, name: '공유' },
+  { id: 2, image: sampleImage, name: '배두나' },
+  { id: 3, image: sampleImage, name: '박소담' },
+  { id: 4, image: sampleImage, name: '이동욱' },
+];
