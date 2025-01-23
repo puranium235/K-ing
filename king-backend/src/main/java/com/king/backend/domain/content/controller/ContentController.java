e464fa48b8aed87690db46c99f2dfc1984af412b
최은영 <harrysunae@naver.com>
2025-01-23 15:50:07 +0900

    feat: content 도메인 초기 구성

---
+++ king-backend/src/main/java/com/king/backend/domain/content/controller/ContentController.java
@@ -0,0 +1,8 @@
+package com.king.backend.domain.content.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class ContentController {
+}
---
+++ king-backend/src/main/java/com/king/backend/domain/content/entity/Content.java
@@ -0,0 +1,8 @@
+package com.king.backend.domain.content.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Content {
+
+}
