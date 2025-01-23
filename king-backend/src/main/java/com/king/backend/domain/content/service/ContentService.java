c5d4c8ebf7dc33bf9e04712ea4bbd8495c0116e8
최은영 <harrysunae@naver.com>
2025-01-24 08:16:31 +0900

    feat: comment 도메인 초기 구성(어노테이션)

--- king-backend/src/main/java/com/king/backend/domain/content/service/ContentService.java
+++ king-backend/src/main/java/com/king/backend/domain/content/service/ContentService.java
@@ -1,7 +1,11 @@
 package com.king.backend.domain.content.service;
 
+import com.king.backend.domain.content.repository.ContentRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ContentService {
+    private final ContentRepository contentRepository;
 }
