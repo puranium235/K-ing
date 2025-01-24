0e67d4319e3a57830111d3c94625b8647adf6784
이효승 <julia_2000@naver.com>
2025-01-24 09:30:22 +0900

    design: active 상태 버튼 스타일링

--- king/src/components/common/FilterButton.jsx
+++ king/src/components/common/FilterButton.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const FilterButton = ({ buttonName, buttonIcon: Icon, onClickMethod }) => {
-  //   const { buttonName, buttonIcon, onClickMethod } = btnInfo;
-
+const FilterButton = ({ buttonName, buttonIcon: Icon, onClickMethod, $isActive }) => {
   return (
-    <>
-      <ButtonWrapper onClick={onClickMethod}>
-        {Icon && <Icon />}
-        {buttonName}
-      </ButtonWrapper>
-    </>
+    <ButtonWrapper onClick={onClickMethod} $isActive={$isActive} name={buttonName}>
+      {Icon && <Icon />}
+      {buttonName}
+      {buttonName === '필터' && $isActive && <ActiveIndicator />}
+    </ButtonWrapper>
   );
 };
 export default FilterButton;
@@ -21,12 +18,26 @@ const ButtonWrapper = styled.button`
   justify-content: center;
   align-items: center;
   gap: 0.5rem;
+  position: relative;
 
   border: 1px solid ${({ theme }) => theme.colors.Gray2};
   padding: 0.7rem 1rem;
   width: fit-content;
 
   border-radius: 10px;
-  background-color: ${({ theme, $isActive }) =>
-    $isActive ? theme.colors.MainBlue : theme.colors.Gray0};
+  background-color: ${({ theme, $isActive, name }) =>
+    $isActive && name !== '필터' ? theme.colors.MainBlue : theme.colors.White};
+  color: ${({ theme, $isActive, name }) =>
+    $isActive && name !== '필터' ? theme.colors.White : theme.colors.Gray0};
+`;
+
+const ActiveIndicator = styled.span`
+  position: absolute;
+  top: 2px;
+  right: 2px;
+  width: 7px;
+  height: 7px;
+  border-radius: 50%;
+  background-color: red;
+  border: 2px solid ${({ theme }) => theme.colors.White};
 `;
