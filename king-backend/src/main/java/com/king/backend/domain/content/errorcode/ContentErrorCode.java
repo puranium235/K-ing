35177251a0a69d13220bc1ff7f3f705200ef3a6f
최은영 <harrysunae@naver.com>
2025-01-24 17:24:57 +0900

    feat: ContentErrorCode 연결

--- king-backend/src/main/java/com/king/backend/domain/content/errorcode/ContentErrorCode.java
+++ king-backend/src/main/java/com/king/backend/domain/content/errorcode/ContentErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ContentErrorCode implements ErrorCode {
-
+    CONTENT_NOT_FOUND(HttpStatus.NOT_FOUND, "컨텐츠를 찾을 수 없습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,  "서버 내부 에러");
 
     private final HttpStatus status;
--- king-backend/src/main/java/com/king/backend/domain/content/service/ContentService.java
+++ king-backend/src/main/java/com/king/backend/domain/content/service/ContentService.java
@@ -4,7 +4,9 @@ import com.king.backend.domain.cast.entity.Cast;
 import com.king.backend.domain.content.dto.response.ContentDetailResponseDto;
 import com.king.backend.domain.content.entity.Content;
 import com.king.backend.domain.content.entity.ContentCast;
+import com.king.backend.domain.content.errorcode.ContentErrorCode;
 import com.king.backend.domain.content.repository.ContentRepository;
+import com.king.backend.global.exception.CustomException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,7 +21,7 @@ public class ContentService {
 
     @Transactional
     public ContentDetailResponseDto getContentDetail(Long id){
-        Content content = contentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("에러 발생"));
+        Content content = contentRepository.findById(id).orElseThrow(() -> new CustomException(ContentErrorCode.CONTENT_NOT_FOUND));
         List<ContentDetailResponseDto.RelatedCast> relatedCasts = content.getContentCasts().stream()
                 .map(this::mapToRelatedCasts)
                 .collect(Collectors.toList());
