1477823c07d03442bbf9f9f726f29e54592e1602
이효승 <julia_2000@naver.com>
2025-01-31 15:55:21 +0900

    feat: 장르 전환 방식 변경

--- king/src/lib/content.js
+++ king/src/lib/content.js
@@ -9,3 +9,13 @@ export const getContentDetails = async (contentId) => {
     console.error(err);
   }
 };
+
+//연예인 세부정보 조회
+export const getCelebDetails = async (celebId) => {
+  try {
+    const { data } = await client.get(`/cast/${celebId}`);
+    return data.data;
+  } catch (err) {
+    console.error(err);
+  }
+};
