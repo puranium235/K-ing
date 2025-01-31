85c91e0801e81b1ce525c7210c1f4251a8bbd11c
이하경 <puranium235@gmail.com>
2025-01-31 15:52:49 +0900

    style: 에러코드 문구 수정

--- king-backend/src/main/java/com/king/backend/domain/curation/errorcode/CurationErrorCode.java
+++ king-backend/src/main/java/com/king/backend/domain/curation/errorcode/CurationErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum CurationErrorCode implements ErrorCode {
-    CURATION_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 큐레이션 id입니다.");
+    CURATION_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 큐레이션 리스트를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
