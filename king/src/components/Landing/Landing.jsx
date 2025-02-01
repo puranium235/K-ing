433d5cde585802736d80c8ae4285532e7b4fe5d7
김민경 <kimmg0708@naver.com>
2025-02-01 15:41:50 +0900

    feat: 회원정보입력페이지 번역기능 추가

--- king/src/components/Landing/Landing.jsx
+++ king/src/components/Landing/Landing.jsx
@@ -12,48 +12,49 @@ const Landing = () => {
   };
 
   return (
-    <St.Page>
+    <StLandingWrapper>
       <St.ContentWrapper>
         <St.IconWrapper>
           <IcKing />
         </St.IconWrapper>
         <St.TaglineWrapper>
-          <St.Tagline>드라마의 발자취를 따라 주인공이 되어보세요.</St.Tagline>
-          <St.Title>촬영 배경지 큐레이션 서비스</St.Title>
+          <Tagline>드라마의 발자취를 따라 주인공이 되어보세요.</Tagline>
+          <Title>촬영 배경지 큐레이션 서비스</Title>
         </St.TaglineWrapper>
-        <St.BrandName>K-ing</St.BrandName>
+        <BrandName>K-ing</BrandName>
       </St.ContentWrapper>
       <St.FootprintWrapper>
         <IcFootsteps />
       </St.FootprintWrapper>
       <St.ButtonWrapper>
-        <St.SocialButton $google onClick={googleLogin}>
+        <SocialButton $google onClick={googleLogin}>
           <img src={GoogleIcon} alt="구글 아이콘" />
           Continue with Google
-        </St.SocialButton>
-        <St.SocialButton $line>
+        </SocialButton>
+        <SocialButton $line>
           <img src={LineIcon} alt="라인 아이콘" />
           Continue with LINE
-        </St.SocialButton>
+        </SocialButton>
       </St.ButtonWrapper>
-    </St.Page>
+    </StLandingWrapper>
   );
 };
 
 export default Landing;
 
-const St = {
-  Page: styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+const StLandingWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
-    gap: 5rem;
-    position: relative;
-    height: 100vh;
-    background-color: ${({ theme }) => theme.colors.White || '#fff'};
-  `,
+  gap: 5rem;
+  position: relative;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.colors.White};
+`;
+
+const St = {
   ContentWrapper: styled.div`
     display: flex;
     flex-direction: column;
@@ -69,22 +70,6 @@ const St = {
     flex-direction: column;
     align-items: center;
     gap: 5px; /* 줄 간격 */
-  `,
-  Tagline: styled.p`
-    ${({ theme }) => theme.fonts.Body4 || 'font-size: 1rem;'}
-    color: ${({ theme }) => theme.colors.Gray2 || '#666'};
-    text-align: center;
-  `,
-  Title: styled.h1`
-    ${({ theme }) => theme.fonts.Title2 || 'font-size: 1.5rem;'}
-    font-weight: bold;
-    text-align: center;
-  `,
-  BrandName: styled.h2`
-    ${({ theme }) => 'font-size: 3rem;'}
-    font-weight: bold;
-    margin-top: 10px;
-    color: ${({ theme }) => theme.colors.Gray0};
   `,
   FootprintWrapper: styled.div`
     /* margin-top: 10px;
@@ -103,29 +88,46 @@ const St = {
     width: 100%;
     max-width: 300px; /* 버튼의 최대 너비 제한 */
   `,
-  SocialButton: styled.button`
-    display: flex;
-    align-items: center;
-    justify-content: flex-start; /* 왼쪽 정렬 */
-    gap: 10px;
-    padding: 15px 20px; /* 왼쪽 여백 추가 */
-    width: 100%;
-    max-width: 300px;
-    border: none;
-    border-radius: 5px;
-    font-size: ${({ theme }) => theme.fonts.Body3};
-    cursor: pointer;
-
-    background-color: ${({ $google }) => ($google ? '#4285F4' : '#00C300')};
-    color: white;
-
-    img {
-      width: 20px;
-      height: 20px;
-    }
-
-    &:hover {
-      opacity: 0.9;
-    }
-  `,
 };
+
+const Tagline = styled.p`
+  ${({ theme }) => theme.fonts.Body4}
+  color: ${({ theme }) => theme.colors.Gray2};
+  text-align: center;
+`;
+const Title = styled.h1`
+  ${({ theme }) => theme.fonts.Title2}
+  font-weight: bold;
+  text-align: center;
+`;
+const BrandName = styled.h2`
+  ${({ theme }) => 'font-size: 3rem;'}
+  font-weight: bold;
+  margin-top: 10px;
+  color: ${({ theme }) => theme.colors.Gray0};
+`;
+const SocialButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start; /* 왼쪽 정렬 */
+  gap: 10px;
+  padding: 15px 20px; /* 왼쪽 여백 추가 */
+  width: 100%;
+  max-width: 300px;
+  border: none;
+  border-radius: 5px;
+  font-size: ${({ theme }) => theme.fonts.Body3};
+  cursor: pointer;
+
+  background-color: ${({ $google }) => ($google ? '#4285F4' : '#00C300')};
+  color: white;
+
+  img {
+    width: 20px;
+    height: 20px;
+  }
+
+  &:hover {
+    opacity: 0.9;
+  }
+`;
