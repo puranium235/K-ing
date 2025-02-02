b40dfb53f79c8394ec5a4831e9f50029713290d6
hyoseung2000 <hyoseung.lee2000@gmail.com>
2025-02-02 19:14:01 +0900

    chore: 연예인 타입 명칭 수정

--- king/src/Router.jsx
+++ king/src/Router.jsx
@@ -51,7 +51,7 @@ const Router = () => {
             <Route path="/curation/map" element={<CurationMapPage />} />
             <Route path="/feed" element={<FeedPage />} />
             <Route path="/content/:contentType" element={<ContentPage />} />
-            <Route path="/content/celeb/:celebId" element={<CelebDetailPage />} />
+            <Route path="/content/cast/:celebId" element={<CelebDetailPage />} />
             <Route path="/content/detail/:contentId" element={<ContentDetailPage />} />
             {/* 카테고리 설정시 */}
             <Route path="/search/keyword" element={<SearchKeywordpage />} />
