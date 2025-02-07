8ec35873d39315c65a8d96d9a47488ff36f86b52
이효승 <julia_2000@naver.com>
2025-02-07 09:27:53 +0900

    feat: 장소 검색 무한 스크롤

---
+++ king/src/hooks/search/useGetPlaceSearchResult.js
@@ -0,0 +1,39 @@
+import { useEffect } from 'react';
+import useSWRInfinite from 'swr/infinite';
+
+import { mainGetFetcher } from '../../lib/axios';
+
+const useGetPlaceSearchResult = (query, category) => {
+  const getKey = (pageIndex, previousPageData) => {
+    if (previousPageData && !previousPageData.data.nextCursor) return null; // 마지막 페이지
+
+    return pageIndex === 0
+      ? `/search/search?query=${encodeURIComponent(query)}&category=${category}&size=15`
+      : `/search/search?query=${encodeURIComponent(query)}&category=${category}&size=15&cursor=${previousPageData.data.nextCursor}`;
+  };
+
+  const { data, error, size, setSize, isValidating, mutate } = useSWRInfinite(
+    getKey,
+    mainGetFetcher,
+  );
+
+  useEffect(() => {
+    mutate();
+    setSize(1);
+  }, [query, category, mutate, setSize]);
+
+  const results = data ? [].concat(...data.map((res) => res.data.results)) : [];
+
+  return {
+    searchResultList: results,
+    getNextData: () => {
+      if (!isValidating) {
+        setSize(size + 1);
+      }
+    },
+    isLoading: isValidating,
+    isError: error,
+  };
+};
+
+export default useGetPlaceSearchResult;
