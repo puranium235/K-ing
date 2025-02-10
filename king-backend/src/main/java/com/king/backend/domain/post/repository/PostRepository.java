d4599422b05628dc1bca093dd8cc0f7ab5809423
최은영 <harrysunae@naver.com>
2025-02-10 13:13:48 +0900

    feat: 타사용자 마이페이지

--- king-backend/src/main/java/com/king/backend/domain/post/repository/PostRepository.java
+++ king-backend/src/main/java/com/king/backend/domain/post/repository/PostRepository.java
@@ -14,13 +14,13 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     // 홈피드
     List<Post> findAllByIsPublicOrderByIdDesc(boolean isPublic, Pageable pageable);
     List<Post> findByIsPublicAndIdLessThanOrderByIdDesc(boolean isPublic, Long lastId, Pageable pageable);
-    // 장소리뷰 - 인기순
-//    List<Post> findAllByIsPublicAndPlaceOrderByLikesCntDescIdDesc(boolean isPublic, Place place, Pageable pageable);
-//    List<Post> findByIsPublicAndPlaceAndIdLessThanOrderByLikesCntDescIdDesc(boolean isPublic, Place place, Long lastId, Pageable pageable);
     // 장소리뷰 - 최신순
     List<Post> findByIsPublicAndPlaceOrderByIdDesc(boolean isPublic, Place place, Pageable pageable);
     List<Post> findByIsPublicAndPlaceAndIdLessThanOrderByIdDesc(boolean isPublic, Place place, Long lastId, Pageable pageable);
     // 마이페이지
     List<Post> findAllByWriterOrderByIdDesc(User user, Pageable pageable);
     List<Post> findByWriterAndIdLessThanOrderByIdDesc(User user, Long lastId, Pageable pageable);
+    // 타사용자 마이페이지
+    List<Post> findAllByIsPublicAndWriterOrderByIdDesc(boolean isPublic, User user, Pageable pageable);
+    List<Post> findByIsPublicAndWriterAndIdLessThanOrderByIdDesc(boolean isPublic, User user, Long lastId, Pageable pageable);
 }
