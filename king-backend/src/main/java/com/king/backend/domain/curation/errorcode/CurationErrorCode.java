0619241a3535ec033259ee9831d01db10bf169f6
이하경 <puranium235@gmail.com>
2025-02-11 19:06:58 +0900

    feat: 북마크 API

--- king-backend/src/main/java/com/king/backend/domain/curation/errorcode/CurationErrorCode.java
+++ king-backend/src/main/java/com/king/backend/domain/curation/errorcode/CurationErrorCode.java
@@ -12,6 +12,7 @@ public enum CurationErrorCode implements ErrorCode {
     INVALID_VALUE(HttpStatus.BAD_REQUEST, "필수값이 없거나 글자수 제한을 초과했습니다"),
     INVALID_IMAGEFILE(HttpStatus.BAD_REQUEST, "배경 이미지가 없습니다"),
     DUPLICATED_PLACE(HttpStatus.CONFLICT, "중복된 장소가 있습니다"),
+    DUPLICATED_BOOKMARK(HttpStatus.CONFLICT, "이미 북마크 된 큐레이션입니다"),
     FORBIDDEN_CURATION(HttpStatus.FORBIDDEN, "유저의 큐레이션이 아닙니다");
 
     private final HttpStatus status;
