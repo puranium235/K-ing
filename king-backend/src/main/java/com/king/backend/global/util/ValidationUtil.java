a510dcc185d5ca022a3c26b4955646764dcf7a45
이하경 <puranium235@gmail.com>
2025-02-12 12:50:22 +0900

    fix: 큐레이션 제목 150자까지로 수정

--- king-backend/src/main/java/com/king/backend/global/util/ValidationUtil.java
+++ king-backend/src/main/java/com/king/backend/global/util/ValidationUtil.java
@@ -2,6 +2,6 @@ package com.king.backend.global.util;
 
 public class ValidationUtil {
     public static boolean checkNotNullAndLengthLimit(String str, int length) {
-        return str != null && !str.trim().isEmpty() && str.length() < length;
+        return str != null && !str.trim().isEmpty() && str.length() <= length;
     }
 }
