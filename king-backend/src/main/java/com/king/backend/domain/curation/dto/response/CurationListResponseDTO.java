c706910d285260d92ae7e634e5b2c11ccab88b33
이하경 <puranium235@gmail.com>
2025-02-14 15:15:01 +0900

    fix: 북마크 큐레이션 조회 분리

--- king-backend/src/main/java/com/king/backend/domain/curation/dto/response/CurationListResponseDTO.java
+++ king-backend/src/main/java/com/king/backend/domain/curation/dto/response/CurationListResponseDTO.java
@@ -1,31 +1,21 @@
 package com.king.backend.domain.curation.dto.response;
 
-import com.king.backend.domain.curation.entity.CurationList;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
-import java.util.Set;
 
 @Getter
-@AllArgsConstructor
+@Setter
+@Builder
 public class CurationListResponseDTO {
     private List<CurationDTO> curations;
     private String nextCursor;
 
-    public static CurationListResponseDTO fromEntity(List<CurationList> curations, Set<Long> bookmarkedCurationIds, String nextCursor) {
-        return new CurationListResponseDTO(
-                curations.stream()
-                        .map(curation -> CurationDTO.fromEntity(curation, bookmarkedCurationIds.contains(curation.getId())))
-                        .toList(),
-                nextCursor
-        );
-    }
-
     @Getter
     @Setter
-    @AllArgsConstructor
+    @Builder
     public static class CurationDTO {
         private Long curationId;
         private String title;
@@ -33,16 +23,5 @@ public class CurationListResponseDTO {
         private String writerNickname;
         private boolean isPublic;
         private boolean bookmarked;
-
-        public static CurationDTO fromEntity(CurationList curation, boolean bookmarked) {
-            return new CurationDTO(
-                    curation.getId(),
-                    curation.getTitle(),
-                    curation.getImageUrl(),
-                    curation.getWriter().getNickname(),
-                    curation.isPublic(),
-                    bookmarked
-            );
-        }
     }
 }
