c3ef9badba89737037385d60e578eb75f1adc634
최은영 <harrysunae@naver.com>
2025-02-16 15:24:59 +0900

    feat: 댓글알림만

--- king-backend/src/main/java/com/king/backend/domain/fcm/service/FcmTokenService.java
+++ king-backend/src/main/java/com/king/backend/domain/fcm/service/FcmTokenService.java
@@ -4,8 +4,6 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
-import com.king.backend.domain.curation.entity.CurationList;
-import com.king.backend.domain.curation.repository.CurationListRepository;
 import com.king.backend.domain.fcm.entity.FcmToken;
 import com.king.backend.domain.fcm.repository.FcmTokenRepository;
 import com.king.backend.domain.user.dto.domain.OAuth2UserDTO;
@@ -15,12 +13,10 @@ import com.king.backend.domain.user.repository.UserRepository;
 import com.king.backend.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -29,7 +25,6 @@ import java.util.Optional;
 public class FcmTokenService {
     private final FcmTokenRepository fcmTokenRepository;
     private final UserRepository userRepository;
-    private final CurationListRepository curationListRepository;
 
     public void registerToken(String token) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -58,39 +53,6 @@ public class FcmTokenService {
                         .build())
                 .build();
         return FirebaseMessaging.getInstance().send(message);
-    }
-
-    /**
-     * 매일 08:00, 16:00에 가장 최근 큐레이션을 추천
-     */
-//    @Scheduled(cron = "0 0 8,16 * * *", zone = "Asia/Seoul")
-    @Scheduled(fixedRate = 10000)
-    public void sendCurationRecommendations() {
-        Optional<CurationList> optionalCuration = curationListRepository.findTopByIsPublicTrueOrderByCreatedAtDesc();
-        if (optionalCuration.isEmpty()) {
-            log.info("큐레이션이 존재하지 않습니다.");
-            return;
-        }
-        CurationList latestCuration = optionalCuration.get();
-
-        String title = "큐레이션 추천";
-        String body = "'" + latestCuration.getTitle() + "'" + " 큐레이션을 확인해보세요.";
-
-        List<FcmToken> tokens = fcmTokenRepository.findAll();
-        if (tokens.isEmpty()) {
-            log.info("전송할 FCM 토큰이 없습니다.");
-            return;
-        }
-        log.info("3초마다 출력 중");
-
-        for (FcmToken tokenEntity : tokens) {
-            try {
-                String responseId = sendMessageByToken(tokenEntity.getToken(), title, body);
-                log.info("토큰 {}: 알림 전송 성공 (메시지 ID: {})", tokenEntity.getToken(), responseId);
-            } catch (FirebaseMessagingException e) {
-                log.error("토큰 {}: 알림 전송 실패 - {}", tokenEntity.getToken(), e.getMessage());
-            }
-        }
     }
 
     public void deleteToken(String token) {
