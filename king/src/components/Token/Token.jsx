a78603900ff6c87fe599a9ae07d64014210acec4
이효승 <julia_2000@naver.com>
2025-02-17 08:17:00 +0900

    feat: ios 관련 권한요청 위치 수정

--- king/src/components/Token/Token.jsx
+++ king/src/components/Token/Token.jsx
@@ -28,7 +28,7 @@ const Token = () => {
             alert(
               '다음의 실행 환경을 권장합니다.\n권장 테스트 환경 : PWA\n아이폰 : 공유 - 홈 화면에 추가\n안드로이드 : 우상단 메뉴 - 홈 화면에 추가',
             );
-            handleAllowNotification();
+            // handleAllowNotification();
             navigate('/home'); // 홈 페이지로 이동
           } else {
             navigate('/'); // 잘못된 값이 들어오면 메인 페이지로 이동
