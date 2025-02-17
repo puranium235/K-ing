8ebbb60c01a7388152e5c17ccf80c0b0b5bb0008
이효승 <julia_2000@naver.com>
2025-02-17 12:28:36 +0900

    feat: 랭킹 순위 고정

--- king/src/components/Home/Rank.jsx
+++ king/src/components/Home/Rank.jsx
@@ -74,12 +74,18 @@ const Rank = () => {
   }, [rankingsData, currentRankSet]);
 
   useEffect(() => {
+    setCurrentRankSet(0);
+
     const intervalId = setInterval(() => {
-      setCurrentRankSet((prev) => (prev === 0 ? 1 : 0));
+      if (rankingsData.length > 4) {
+        setCurrentRankSet((prev) => (prev === 0 ? 1 : 0));
+      } else {
+        setCurrentRankSet(0);
+      }
     }, 3000);
 
     return () => clearInterval(intervalId);
-  }, []);
+  }, [rankingsData]);
 
   const handleClickTrend = (keyword) => {
     setQuery(keyword);
