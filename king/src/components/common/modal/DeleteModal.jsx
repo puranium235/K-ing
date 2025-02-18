a0de965d83e1f91641e106c1279cf7bc611914c8
이효승 <julia_2000@naver.com>
2025-02-18 18:47:16 +0900

    feat: 삭제 모달 수정중..

--- king/src/components/common/modal/DeleteModal.jsx
+++ king/src/components/common/modal/DeleteModal.jsx
@@ -1,37 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import SmallModal from '../../common/modal/smallModal';
+
 const DeleteModal = ({ isOpen, onClose, onConfirm }) => {
   if (!isOpen) return null; // 모달이 열려 있지 않으면 렌더링 안 함
 
   return (
-    <ModalOverlay>
-      <ModalContent>
-        <p>정말 삭제하시겠습니까?</p>
-        <ButtonGroup>
-          <button onClick={onConfirm}>확인</button>
-          <button onClick={onClose}>취소</button>
-        </ButtonGroup>
-      </ModalContent>
-    </ModalOverlay>
+    <SmallModal>
+      <p>정말 삭제하시겠습니까?</p>
+      <ButtonGroup>
+        <button onClick={onConfirm}>확인</button>
+        <button onClick={onClose}>취소</button>
+      </ButtonGroup>
+    </SmallModal>
   );
 };
 
 export default DeleteModal;
-
-// ✅ 모달 스타일
-const ModalOverlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.5);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  z-index: 1000;
-`;
 
 const ModalContent = styled.div`
   background: white;
