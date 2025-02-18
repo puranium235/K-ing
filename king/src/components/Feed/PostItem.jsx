00bce1d2a312a1d28c11d3ffc4b1f3fd72299593
김민경 <kimmg0708@naver.com>
2025-02-18 17:52:59 +0900

    feat: 목록에서 게시글 좋아요 추가

--- king/src/components/Feed/PostItem.jsx
+++ king/src/components/Feed/PostItem.jsx
@@ -1,15 +1,36 @@
-import { forwardRef } from 'react';
+import { forwardRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { IcComments, IcHeartTrue, IcLikes } from '../../assets/icons';
+import { likePost, unLikePost } from '../../lib/post';
 import { formatDate } from '../../util/formatDate';
 
 const PostItem = forwardRef(({ post, column }, ref) => {
   const navigate = useNavigate();
+  const [isLiked, setIsLiked] = useState(post.liked);
+  const [likesCount, setLikesCount] = useState(post.likesCnt);
 
   const handleFeedClick = (id) => {
     navigate(`/feed/${id}`);
+  };
+
+  const handleLikeClick = async (event) => {
+    event.stopPropagation();
+
+    if (isLiked) {
+      const res = await unLikePost(post.postId);
+      if (res.success) {
+        setIsLiked(false);
+        setLikesCount((prev) => prev - 1);
+      }
+    } else {
+      const res = await likePost(post.postId);
+      if (res.success) {
+        setIsLiked(true);
+        setLikesCount((prev) => prev + 1);
+      }
+    }
   };
 
   return (
@@ -19,9 +40,9 @@ const PostItem = forwardRef(({ post, column }, ref) => {
       </ImageWrapper>
       <St.Info>
         <St.Action>
-          <IconText>
-            {post.liked ? <IcHeartTrue /> : <IcLikes />}
-            <p>{post.likesCnt}</p>
+          <IconText onClick={handleLikeClick}>
+            {isLiked ? <IcHeartTrue /> : <IcLikes />}
+            <p>{likesCount}</p>
           </IconText>
           <IconText>
             <IcComments />
