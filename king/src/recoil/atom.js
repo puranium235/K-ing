c9dbad608a7070fa01c318ab24c84854f8f9a41e
이효승 <julia_2000@naver.com>
2025-02-19 13:19:02 +0900

    feat: 자동완성시 예외처리 및 search 요청 전송

--- king/src/recoil/atom.js
+++ king/src/recoil/atom.js
@@ -36,6 +36,11 @@ const SearchQueryState = atom({
   default: '',
 });
 
+const PlaceSearchQueryState = atom({
+  key: 'placeSearchQueryState',
+  default: '',
+});
+
 const SearchRelatedType = atom({
   key: 'searchRelatedType',
   default: '',
@@ -122,6 +127,7 @@ export {
   CurationPlaceUploadList,
   DraftExist,
   FilterOption,
+  PlaceSearchQueryState,
   ProfileState,
   ScrollPosition,
   SearchCategoryState,
